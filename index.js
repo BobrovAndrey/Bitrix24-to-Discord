6b742ed43cc910d8a2f6bbb2c5ac9ff130135036
Andrey Bobrov <Bobrov.law@gmail.com>
2018-12-22 15:54:29 +0200

    0.2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
-* From bitrix24 to discord channel data poster
+* From bitrix24 to discord channel data courier
 * ver 0.1  
 *
 *
@@ -10,11 +10,16 @@
   const http = require('http');
   const url = require('url');
   const StringDecoder = require('string_decoder').StringDecoder;
-  const BX24 = require ('b24');
+  // const querystring = require('querystring');
+  const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 // Init the server 
 
 let server = {};
+
+//Init HTTP request
+let xhr = new XMLHttpRequest();
+
 
 // Init the HTTPS server
 
@@ -28,61 +33,76 @@ server = http.createServer(function(req,res){
 
 
   //Get the query string
-  let queryStringObj = JSON.stringify(parsedUrl.query); // Undefined
+  let queryStringObj = parsedUrl.query; // Undefined
   
   // Get the headers
-  let headers = JSON.stringify(req.headers);
+  let headers = req.headers;
 
   //Get the HTTP method
   let method = req.method.toLowerCase();
 
   //Get the payload 
   const decoder = new StringDecoder('utf-8');
-  let buffer = '';
+  // let buffer = '';
+  // req.on('data', (data) => {
+  //   buffer += decoder.write(data);
+  // });
+  // req.on('end',() => {
+  //   buffer += decoder.end();
+  //   let finalBuffer = querystring.parse(buffer);
+
+    let buffer = '';
   req.on('data', (data) => {
-    buffer += decoder.write(data);
+    buffer += data;
   });
+
   req.on('end',() => {
-    buffer += decoder.end();
-    let finalBuffer = Object.entries(buffer);
-    
+    // let finalBuffer = querystring.parse (buffer);
+    // let leadID = Object.values(finalBuffer)[1];
+    let leadId = Object.values(querystring.parse(buffer))[1];
+    let requestBody = {
+      "name": "Bitrix24LeadHook",
+      "channel_id": "525295956720222238",
+      "token": "Soicvoi3qNA7FS8XDQGB8xYi117rpm20hIqOleG0Bhn65HsZK2yLCGTf8utg3x3mErXM",
+      "avatar": "https://m.bitrix24.ru/images/b24_logo_mobile_ru.png",
+      "guild_id": "525295956720222234",
+      "id": "525296886060679169",
+      "content" : leadId
+    };
+    let requestBodyTrue = JSON.stringify(requestBody); 
 
-    console.log(`Request method: ${ method}`);
-    console.log(`Path is: ${ path}`);
-    console.log(`Query String Object: ${ queryStringObj}`);
-    console.log(`Request received with headers: ${ headers}`);
-    console.log(`Request received with payload: ${ buffer}`);
-    console.log(`---------------------------------------------------------------------`);
-    console.log(`Request received with payload: ${ finalBuffer}`);
+    // console.log(`Request method: ${ method}`);
+    // console.log(`Path is: `,  path);
+    // console.log(`Query String Object`, queryStringObj);
+    // console.log(`Request received with headers: ${ headers}`);
+    // console.log(`Request received with payload: ${ buffer}`);
+    // console.log(`---------------------------------------------------------------------`);
+    // console.log(finalBuffer);
+    // console.log(`---------------------------------------------------------------------`);
+    // console.log(Object.keys(finalBuffer));
+    console.log('Lead ID is: ',leadId);
 
+    xhr.open('POST','https://discordapp.com/api/webhooks/525296886060679169/Soicvoi3qNA7FS8XDQGB8xYi117rpm20hIqOleG0Bhn65HsZK2yLCGTf8utg3x3mErXM',false);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+   
+    console.log(requestBodyTrue);
 
-    BX24.callMethod("crm.lead.get", { id: '1b87798bc2d16c4b7f2a604e0c899a34' }, function(result) {
-                      if(result.error())
-                          console.error(result.error());
-                      else
-                          console.dir(result.data());
-    });
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          console.log('Success:', xhr.readyState);
+        } else {
+          console.log('Error', xhr.readyState);
+        }
+      }
+
+    xhr.send(requestBodyTrue);
   });
-
 });
 
 //Start the server + tell him to listen
 server.listen(80, function(){
   console.log('The server is listening on port 80 now');
 });
-
-
-// var id = prompt("Enter ID");
-//         BX24.callMethod(
-//             "crm.lead.get", 
-//             { id: id }, 
-//             function(result) 
-//             {
-//                 if(result.error())
-//                     console.error(result.error());
-//                 else
-//                     console.dir(result.data());
-//             }
 
 
 
