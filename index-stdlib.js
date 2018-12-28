2935219b6d6ee914051eac665cf578f80f0a5c1f
Andrey Bobrov <Bobrov.law@gmail.com>
2018-12-28 17:26:48 +0200

    Node-fetch using

--- index-stdlib.js
+++ index-stdlib.js
@@ -2,67 +2,22 @@
 * Bitrix24 to Discord data courier
 */
 
-const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
+const fetch = require('node-fetch')
 
 module.exports = async function (context) {
-  // Dependencies
+
   console.log('Hello, it`/s bitrix24 to Discord data courier\n')
-  // const newPost = (post) => {
-  //   let options = {
-  //     method: 'POST',
-  //     'embeds': [
-  //       {
-  //         'title': `New lead #${incomingLeadId}`,
-  //         // 'description': 'Request for invoice',
-  //         'url': incomingLeadId,
-  //         'author': {
-  //           'name': 'ACME Inc.'
-  //         }
-  //       }
-  //     ],
-  //     headers: {
-  //       'Content-Type': 'application/x-www-form-urlencoded'
-  //     }
-  //   }
-  //   return got('https://discordapp.com/api/webhooks/525296886060679169/Soicvoi3qNA7FS8XDQGB8xYi117rpm20hIqOleG0Bhn65HsZK2yLCGTf8utg3x3mErXM', options)
-  //     .then(console.log('SEND'))
-  //     .cath(error => console.error(`Error ${error}`))
-  // }
+  console.log('This is a new request')
 
   let incomingLeadId = context.params['data[FIELDS][ID]']
 
   console.log(typeof (incomingLeadId))
-  
+
   incomingLeadId = incomingLeadId !== undefined && typeof (incomingLeadId) === 'string' ? incomingLeadId : false
   if (incomingLeadId) {
-    let xhr = new XMLHttpRequest()
-    console.log('1')
-    xhr.open('POST', 'https://discordapp.com/api/webhooks/525296886060679169/Soicvoi3qNA7FS8XDQGB8xYi117rpm20hIqOleG0Bhn65HsZK2yLCGTf8utg3x3mErXM', false)
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-    console.log('2')
-    let payload = {
-      'embeds': [
-        {
-          'title': `New lead #${incomingLeadId}`,
-          // 'description': 'Request for invoice',
-          'url': incomingLeadId,
-          'author': {
-            'name': 'ACME Inc.'
-          }
-        }
-      ]
-    }
-    console.log('3')
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4 && xhr.status === 204) {
-        console.log(`Notification for lead ${incomingLeadId} was sent successfully with payload: `, payload)
-      } else {
-        throw new Error(`Sending data to Discord unsuccessful.\nXHR readyState: ${xhr.readyState}\nXHR status: ${xhr.status}`)
-      }
-    }
-    console.log('4')
-    xhr.send(JSON.stringify(incomingLeadId))
-    console.log('5')
+    fetch('https://discordapp.com/api/webhooks/525296886060679169/Soicvoi3qNA7FS8XDQGB8xYi117rpm20hIqOleG0Bhn65HsZK2yLCGTf8utg3x3mErXM', { method: 'POST', body: 'a=1' })
+      .then(res => res.json()) // expecting a json response
+      .then(json => console.log(json))
   } else {
     throw new Error(`Error, requst did not contain any lead id information of lead id information type did not mutch "string",${incomingLeadId}`)
   }
