e0719fa8f6bfb8d9dd94d18824db5085dff25ae6
Andrey <42993931+BobrovAndrey@users.noreply.github.com>
2019-01-02 20:56:57 +0200

    Add task comment

--- index-stdlib.js
+++ index-stdlib.js
@@ -36,3 +36,32 @@ module.exports = async function (context) {
     console.error('Error, while asign "incomingLeadId"', error)
   }
 }
+
+/*
+// bitrix-to-discord-notify
+
+const discord = require('discord-sender')
+const bitrixParser = require('bitrix-webhook-parser')
+const bitrix = require('@2bad/iris.crm.bitrix)
+
+module.exports = async function (context) {
+    let { leadId, bitrixDomain } = bitrixParser.parseWebhookBody(context.params)
+    let leadData = bitrix.fetch('crm.lead.find', leadId)
+
+    let payload = {leadData.id, leadData.Name}
+
+    await discord.send(WEBHOOK_URL, payload )
+    await sms.send(NUMBER, payload)
+}
+
+
+
+let parseWebhookData = function (data) {
+    return {
+        leadId: data['data[FIELDS][ID]']
+        ...
+
+    }
+}
+
+*/
