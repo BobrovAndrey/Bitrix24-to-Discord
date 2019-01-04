7413ca166b56471fb77e8394b75dfff6dc3ce97d
Andrey Bobrov <Bobrov.law@gmail.com>
2019-01-04 13:00:21 +0200

    bitrix-parser fix

--- bitrix-parser.js
+++ bitrix-parser.js
@@ -1,4 +1,6 @@
 let parser = {}
+
+// Parse data from bitrix
 parser.data = function (incomingData) {
   return {
     'leadId': incomingData.params['data[FIELDS][ID]'],
@@ -12,49 +14,28 @@ parser.data = function (incomingData) {
 }
 
 // Form payload data
-parser.Payload = function (name, channelId, token, avatar, guildId, id, content) {
-  name = name || null
-  channelId = channelId || null
-  token = token || null
-  avatar = avatar || null
-  guildId = guildId || null
-  id = id || null
-  content = content || null
-
+parser.payload = function (name, channelId, token, avatar, guildId, id, content) {
   return {
-    'name': `${name}`,
-    'channel_id': `${channelId}`,
-    'token': `${token}`,
-    'avatar': `${avatar}`,
-    'guild_id': `${guildId}`,
-    'id': `${id}`,
-    'content': `${content}`
+    'name': name, // 'Bitrix24 to Discord data courier',
+    'channel_id': channelId, // '525295956720222238',
+    'token': token, // 'Soicvoi3qNA7FS8XDQGB8xYi117rpm20hIqOleG0Bhn65HsZK2yLCGTf8utg3x3mErXM',
+    'avatar': avatar, // 'http://www.ceo.ru/files/news/news_pics/410.png',
+    'guild_id': guildId, // '525295956720222234',
+    'id': id, // '525296886060679169',
+    'content': content // `Lead with ID ${leadId} was created at your Bittrex24 ${bitrixDomain} account`
   }
 }
-
-parser.Embads = function (title, url, name) {
-  title = title || null
-  url = url || null
-  name = name || null
+// Form embeds data
+parser.embeds = function (title, url, name) {
   return { 'embeds': [
     {
-      'title': `${title}`, // `New lead was recently created #${leadId}`
-      'url': `${url}`, // `https://${bitrixDomain}/crm/lead/details/${leadId}/`
+      'title': title, // `New lead was recently created #${leadId}`
+      'url': url, // `https://${bitrixDomain}/crm/lead/details/${leadId}/`
       'author': {
-        'name': `${name}` // bitrixDomain
+        'name': name // bitrixDomain
       }
     }
   ] }
 }
 
 module.exports = parser
-
-// {
-//   'name': 'Bitrix24 to Discord data courier',
-//   'channel_id': '525295956720222238',
-//   'token': 'Soicvoi3qNA7FS8XDQGB8xYi117rpm20hIqOleG0Bhn65HsZK2yLCGTf8utg3x3mErXM',
-//   'avatar': 'http://www.ceo.ru/files/news/news_pics/410.png',
-//   'guild_id': '525295956720222234',
-//   'id': '525296886060679169',
-//   'content': `Lead with ID ${leadId} was created at your Bittrex24 ${bitrixDomain} account`
-// }
