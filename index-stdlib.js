07a510f6f54dfa70d5f2711a70b3b5c7b65e70d6
Andrey <42993931+BobrovAndrey@users.noreply.github.com>
2019-01-07 14:39:36 +0200

    Try catch only around parser

--- index-stdlib.js
+++ index-stdlib.js
@@ -3,15 +3,16 @@ const { post } = require('../bitrix-sender')
 const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL
 
 module.exports = async function (context) {
+  let leadId, bitrixDomain
   try {
-    const leadId = data(context).leadId
-    const bitrixDomain = data(context).bitrixDomain
-
-    let payload = embeds(`New lead was recently created #${leadId}`, `https://${bitrixDomain}/crm/lead/details/${leadId}`, bitrixDomain)
-    await post(DISCORD_WEBHOOK_URL, payload)
-      .then(responce => console.log(responce))
-      .catch(err => console.log(err))
+    leadId = data(context).leadId
+    bitrixDomain = data(context).bitrixDomain
   } catch (error) {
     throw new Error(error)
   }
+
+  let payload = embeds(`New lead was recently created #${leadId}`, `https://${bitrixDomain}/crm/lead/details/${leadId}`, bitrixDomain)
+  await post(DISCORD_WEBHOOK_URL, payload)
+    .then(responce => console.log(responce))
+    .catch(err => console.log(err))
 }
